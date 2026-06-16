98b6cfd757a1b2accb1e81cd77101ece2324d404
MichiMiau_u1 <ffandedream@gmail.com>
2026-06-16 14:01:43 -0500

    Update restart.js

--- cmds/owner/restart.js
+++ cmds/owner/restart.js
@@ -4,7 +4,7 @@ export default {
   description: 'Reiniciar el socket del bot.',
   isOwner: true,
   run: async ({ msg, sock }) => {
-    await sock.reply(msg.chat, `✎ No... Esperen.*`, msg);
+    await sock.reply(msg.chat, `✎ No... Esperen.`, msg);
     setTimeout(() => {
       if (process.send) {
         process.send("restart");
